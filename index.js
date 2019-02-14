db5d15552147bafc604b3acb76e1270944eb0425
mkay0961 <mkay0961@gmail.com>
2019-02-14 09:33:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file
+function findMatching(driver, name) {
+  let array = []
+
+  for (const x in driver) {
+    if (driver[x].toLowerCase() === name.toLowerCase()) {
+      array.push(driver[x])
+    }
+  }
+  return array
+
+}
+
+function fuzzyMatch(driver, letter) {
+
+  let array = []
+
+  for (const x in driver) {
+
+    if (driver[x].startsWith(letter)) {
+      array.push(driver[x])
+    }
+  }
+  return array
+
+}
+
+function matchName(driver, name) {
+let test = []
+
+  for (let y of driver) {
+
+    let test1 = {}
+    if (y["name"] === name){
+      test1["name"] = y["name"]
+      test1["hometown"] = y["hometown"]
+      test.push(test1)
+    }
+
+    // console.log(test1)
+    console.log("3")
+    console.log(test)
+    console.log("4")
+
+  }
+  return test
+
+}
