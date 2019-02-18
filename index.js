231c4b118bccc01d6d44004a32337bd0c3292ae0
Ben Yellin <byellindc@gmail.com>
2019-02-18 14:21:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function findMatching(drivers, str) {
+  return drivers.filter(d => d.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(d => d.match(new RegExp(`^${str}`, 'gi')));
+}
+
+function matchName(drivers, str) {
+  return drivers.filter(d => d.name.toLowerCase() === str.toLowerCase());
+}
+
