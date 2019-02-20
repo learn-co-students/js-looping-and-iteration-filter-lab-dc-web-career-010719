8d580e41403db9032a0f33348f1f7cb1c0dc1a22
Heloise Taillet <heloise96@hotmail.com>
2019-02-20 12:03:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(element => element.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(array, letters){
+  return array.filter(element => element.toLowerCase().substring(0,letters.length) === letters.toLowerCase());
+}
+
+function matchName(object, name){
+  return object.filter(element => element.name.toLowerCase() === name.toLowerCase());
+}
